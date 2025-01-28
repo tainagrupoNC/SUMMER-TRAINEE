692fd29f786c6689340d0fe827bf23d1417a91bf
Taina <taina.bonfim@gruponc.net.br>
2025-01-28 09:19:01 -0300

    Executável Tesseract

--- Main.py
+++ Main.py
@@ -1,19 +1,19 @@
-import pandas as pd
-import matplotlib.pyplot as plt 
-import selenium 
+import pytesseract
+from PIL import Image
 import cv2 
 
 # Carregando a imagem
-imagem_path = 'image_code.jpg'  # Substitua pelo caminho da sua imagem
-imagem = cv2.imread(imagem_path)
+imagem_path = 'image_code.jpg'  # Caminho no qual a imagem está localizada
+imagem = cv2.imread(imagem_path) # Imagem a ser utilizada carregada 
 
-# Verificando se a imagem foi carregada corretamente
-if imagem is None:
-    print("Erro ao carregar a imagem.")
-else:
-    print("Imagem carregada com sucesso.")
+# Carregando a extensão Tesseract 
+caminho = 'Tesseract/tesseract.exe' # Caminho do executável 
+pytesseract.pytesseract.tesseract_cmd = caminho # Executando a biblioteca 
+img = Image.open(imagem_path)  # Abertura da imagem 
+text = pytesseract.image_to_string(img) # Conversão de imagem para string 
+print(text) # Print da mensagem/ código detectado 
 
-
+# Ilustra na tela a imagem em análise 
 cv2.imshow('Imagem de Exemplo', imagem)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
